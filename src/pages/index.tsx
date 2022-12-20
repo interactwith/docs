e6fba7862573248a34738b35dd4a4e96a97a874c
Miroslaw Shpak <bytesbay@icloud.com>
2022-12-20 21:47:44 +0200

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/api/getting-started/intro">
+            to="/docs/api/intro">
             Intro - 5min ⏱️
           </Link>
         </div>
