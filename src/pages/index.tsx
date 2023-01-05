5a4bbeec4a54810024d8c1876809e8d39508260d
Miroslaw Shpak <bytesbay@icloud.com>
2023-01-05 15:46:47 +0200

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,66 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
+function Actions() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+    <div className="main-page">
+      <div className="container main-page-cnt">
+        <div className="action-btn">
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/api/intro">
-            Intro - 5min ⏱️
+            to="/docs/whitepaper/intro"
+          >
+            <div className="title">
+              Whitepaper
+            </div>
+            <div className="description">
+              Learn more about the project
+            </div>
+
+            <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.53033 5.53033C1.23744 5.82322 0.762563 5.82322 0.46967 5.53033C0.176777 5.23744 0.176777 4.76256 0.46967 4.46967L4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.53033 4.46967C9.82322 4.76256 9.82322 5.23744 9.53033 5.53033C9.23744 5.82322 8.76256 5.82322 8.46967 5.53033L5.75 2.81066L5.75 12.5C5.75 12.9142 5.41421 13.25 5 13.25C4.58579 13.25 4.25 12.9142 4.25 12.5L4.25 2.81066L1.53033 5.53033Z" fill="currentColor"/>
+            </svg>
+          </Link>
+        </div>
+        <div className="action-btn">
+          <Link
+            to="/docs/tutorials/intro"
+          >
+            <div className="title">
+              Tutorials
+            </div>
+            <div className="description">
+              Learn more about the project
+            </div>
+
+            <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.53033 5.53033C1.23744 5.82322 0.762563 5.82322 0.46967 5.53033C0.176777 5.23744 0.176777 4.76256 0.46967 4.46967L4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.53033 4.46967C9.82322 4.76256 9.82322 5.23744 9.53033 5.53033C9.23744 5.82322 8.76256 5.82322 8.46967 5.53033L5.75 2.81066L5.75 12.5C5.75 12.9142 5.41421 13.25 5 13.25C4.58579 13.25 4.25 12.9142 4.25 12.5L4.25 2.81066L1.53033 5.53033Z" fill="currentColor"/>
+            </svg>
+          </Link>
+        </div>
+        <div className="action-btn">
+          <Link
+            to="/docs/api/intro"
+          >
+            <div className="title">
+              API
+            </div>
+            <div className="description">
+              Learn more about the project
+            </div>
+
+            <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.53033 5.53033C1.23744 5.82322 0.762563 5.82322 0.46967 5.53033C0.176777 5.23744 0.176777 4.76256 0.46967 4.46967L4.46967 0.46967C4.76256 0.176777 5.23744 0.176777 5.53033 0.46967L9.53033 4.46967C9.82322 4.76256 9.82322 5.23744 9.53033 5.53033C9.23744 5.82322 8.76256 5.82322 8.46967 5.53033L5.75 2.81066L5.75 12.5C5.75 12.9142 5.41421 13.25 5 13.25C4.58579 13.25 4.25 12.9142 4.25 12.5L4.25 2.81066L1.53033 5.53033Z" fill="currentColor"/>
+            </svg>
+
+
           </Link>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
@@ -31,8 +71,8 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Welcome to ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Forget about blockchain complexity">
+      <Actions />
       {/* <main>
         <HomepageFeatures />
       </main> */}
